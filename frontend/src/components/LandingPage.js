1d61f973960186575eb831c574c6b77168873976
MUHAMMAD NUR SOLIHIN BIN MALIK RADZUAN <93577785+solihinmalik@users.noreply.github.com>
2025-05-18 16:42:41 +0800

    Edit Landing Page
    Edit some text

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -11,11 +11,11 @@ export default function LandingPage() {
       <div className="z-10 space-y-4">
         <img src="/rings.png" alt="Olympic Rings" className="mx-auto w-48 h-36" />
         <h1 className="text-4xl font-bold text-gray-800">Olympic 2024</h1>
-        <p className="text-lg text-gray-600">Dashboard by The Developer</p>
+        <p className="text-lg text-gray-600">Olympic 2024 Paynet On-Site Assessment</p>
         <nav className="flex gap-6 justify-center text-gray-700 font-medium">
           <a href="/dashboard/athletes" className="hover:underline">Athletes</a>
-          <a href="/dashboard/medals" className="hover:underline">Country</a>
-          <a href="/dashboard/sports" className="hover:underline">Historical</a>
+          <a href="/dashboard/medals" className="hover:underline">Medals</a>
+          <a href="/dashboard/sports" className="hover:underline">Sports</a>
         </nav>
       </div>
     </div>
